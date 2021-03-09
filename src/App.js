e2c7e930d272cc8e9689e16a7f7037542e763d37
Ivan <ivangutsalenko92@gmail.com>
2021-03-09 20:17:23 +0200

    Added Formik

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import axios from 'axios';
 
-import { Search } from "./components/SearchInput";
-import { ItemList } from "./components/ItemList";
-import { Header } from "./components/Header";
-import { Home } from "./components/Home";
-import { Episods } from "./components/Episods";
-import { Error } from "./components/Error";
-import { Spinner } from "./components/Spinner";
+import { Search } from './components/SearchInput';
+import { ItemList } from './components/ItemList';
+import { Header } from './components/Header';
+import { Home } from './components/Home';
+import { Episods } from './components/Episods';
+import { Error } from './components/Error';
+import { Spinner } from './components/Spinner';
+import { Subscribe } from './components/Subscribe';
 
-import "./App.scss";
-import { Modal } from "./components/Modal";
+import './App.scss';
+import { Modal } from './components/Modal';
 
 const api = axios.create({
-  baseURL: "https://www.breakingbadapi.com",
+  baseURL: 'https://www.breakingbadapi.com',
 });
 
 export const App = () => {
@@ -22,14 +23,20 @@ export const App = () => {
   const [episods, setEpisods] = useState([]);
   const [randomPerson, setRandomPerson] = useState({});
   const [onePerson, setOnePerson] = useState({});
-  const [searchFiled, setSearchFiled] = useState("");
+  const [searchFiled, setSearchFiled] = useState('');
   const [isActive, setIsActive] = useState(null);
   const [isShow, setIsShow] = useState(false);
+  const [showForm, setShowForm] = useState(false);
 
   useEffect(() => {
     getPerson();
     getEpisods();
     getRandomPerson();
+  }, []);
+
+  useEffect(() => {
+    let timer = setTimeout(() => setShowForm(true), 1000);
+    return () => clearTimeout(timer);
   }, []);
 
   const getPerson = () => {
@@ -62,6 +69,10 @@ export const App = () => {
 
   const addPersonToModal = (user) => setOnePerson(user);
 
+  const closeSubscribeWindow = () => {
+    setShowForm(!showForm);
+  };
+
   const filterPerson = person.filter((persons) => {
     return persons.name.toLowerCase().includes(searchFiled.toLowerCase());
   });
@@ -87,12 +98,12 @@ export const App = () => {
               </>
             )}
           />
-
           <Route path="/episods" render={() => episods.length > 0 && <Episods episodInfo={episods} />} />
           <Route component={Error} />
         </Switch>
         {isActive && <Modal check={onHandleCheck} userInfo={onePerson} />}
         {isShow && <Spinner />}
+        {showForm && <Subscribe closeForm={closeSubscribeWindow} />}
       </div>
     </BrowserRouter>
   );
