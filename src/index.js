ba994632b8cbd636f5c8a50024c756ccaa975ea0
Ivan <ivangutsalenko92@gmail.com>
2021-04-27 19:13:55 +0300

    Added Firebase

--- src/index.js
+++ src/index.js
@@ -8,6 +8,19 @@ import thunk from 'redux-thunk';
 import rootReducer from './redux/rootReducer';
 import createSagaMiddleware from 'redux-saga';
 import { rootSaga } from './sagas/root.saga';
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBhN-FZvdzyYyCkru0cKQQImX-2Kh4oNXw',
+  authDomain: 'example-project-40a8e.firebaseapp.com',
+  databaseURL: 'https://example-project-40a8e-default-rtdb.firebaseio.com',
+  projectId: 'example-project-40a8e',
+  storageBucket: 'example-project-40a8e.appspot.com',
+  messagingSenderId: '680812479473',
+  appId: '1:680812479473:web:2cfac1bdfd441af723fa50',
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(rootReducer, applyMiddleware(thunk, sagaMiddleware));
