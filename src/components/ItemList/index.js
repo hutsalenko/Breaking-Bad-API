7bce99f254475c46b2a770b6ce460a2dfcfd69fd
Ivan <ivangutsalenko92@gmail.com>
2021-05-10 11:35:51 +0300

    Add pagination for characters

--- src/components/ItemList/index.js
+++ src/components/ItemList/index.js
@@ -1,14 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card } from '../Card';
+
+import classNames from 'classnames';
 
 import './index.scss';
 
 export const ItemList = ({ persons }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(6);
+  const [activePage, setActivePage] = useState(0);
+
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = persons.slice(indexOfFirstPost, indexOfLastPost);
+
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(persons.length / postsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  const paginate = (pageNumbers) => setCurrentPage(pageNumbers);
+
   return (
-    <div className="card-list">
-      {persons.map((user) => {
-        return <Card key={user.char_id} name={user.name} img={user.img} all={user} />;
-      })}
+    <div className="card-wrapper">
+      <div className="card-list">
+        {currentPosts.map((user) => {
+          return <Card key={user.char_id} name={user.name} img={user.img} all={user} />;
+        })}
+      </div>
+
+      <ul className="card-page">
+        {pageNumbers.map((number, i) => (
+          <li
+            key={number}
+            onClick={() => {
+              paginate(number);
+              setActivePage(i);
+            }}
+            className={classNames({ 'active-page': activePage == i })}
+          >
+            {number}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
