9cde69a3dd00a1756506bb95667d1d0f8dd8464d
ihu <ihu@keepit.com>
2021-05-12 22:02:41 +0300

    Added Read more to Episods component

--- src/components/Episods/index.js
+++ src/components/Episods/index.js
@@ -1,25 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { EpisodCard } from '../EpisodCard';
 import { useSelector } from 'react-redux';
 import './index.scss';
 
 export const Episods = () => {
   const state = useSelector((state) => state.episods.episods);
+  const [episodsQuantity, setEpisodsQuantity] = useState(10);
+  const totalEpisods = state.slice(0, episodsQuantity);
+
+  const increaseQuantity = () => setEpisodsQuantity((prev) => prev + 5);
+
   return (
-    <div className="episod-list">
-      {state.map((user) => {
-        return (
-          <EpisodCard
-            key={user.episode_id}
-            air_date={user.air_date}
-            episode={user.episode}
-            season={user.season}
-            series={user.series}
-            title={user.title}
-            characters={user.characters}
-          />
-        );
-      })}
-    </div>
+    <>
+      <div className="episod-list">
+        {totalEpisods.map((user) => {
+          return (
+            <EpisodCard
+              key={user.episode_id}
+              air_date={user.air_date}
+              episode={user.episode}
+              season={user.season}
+              series={user.series}
+              title={user.title}
+              characters={user.characters}
+            />
+          );
+        })}
+      </div>
+      <button className="episod-btn" onClick={() => increaseQuantity()}>
+        Read More
+      </button>
+    </>
   );
 };
